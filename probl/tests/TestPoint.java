6de91294e4dda4621d1ea8bfeac4fe59f1be18d2
Oleksii Polischuk <polischuk.kiev@gmail.com>
2017-02-07 00:11:56 +0200

    jj

--- probl/tests/TestPoint.java
+++ probl/tests/TestPoint.java
@@ -3,19 +3,14 @@ import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import calculator.App;
 import geometry.Point;
 
 
 public class TestPoint {
 	
    @Test
-   public void constructor() {
-      double lat = 50.388171;
-      double lon = 30.494762;
-      
-      Point t = new Point(lat + 0.0000001, lon + 0.0000001);
-      
-      assertEquals(lat, t.getLatitude(), 0.0000001);
-      assertEquals(lon, t.getLongitude(), 0.0000001);
+   public void distanceTo() {
+     
    }
 }
